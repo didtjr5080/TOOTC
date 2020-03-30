37f2ce00134ef771b4ef3a81df210e94b088e688
didtjr5080 <didtjr5080@naver.aom>
2020-03-30 21:03:32 +0900

    202003302103

--- Assets/Script/drows_attack.cs
+++ Assets/Script/drows_attack.cs
@@ -12,11 +12,25 @@ public class drows_attack : MonoBehaviour
 
     public bool OnAtack;//공격감지
 
+    public GameObject 기본칼;
+    public GameObject 공격준비칼;
+
     public float DrowScale;//드로즈 스캐일 값 
 
     public float AttackDelay;//공격속도 변수
     public bool DoAttackDelay;
     public float SetAttackDelay;
+
+    public float CoolSkill1;//1번 스킬 쿨타임
+    public bool DoSkill1; //스킬1 지속
+    public bool CoolOnSkill1;//스킬1 쿨온
+    public float Skill1SetCool;//스킬1 쿨타임 설정
+    public float OnSkill1Time;//스킬1 지속시간
+    public float SetOnSkill1Time;//스킬1 지속시간설정
+    public bool SendDoSkill1;
+    public bool OnSkill1;
+
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +41,12 @@ public class drows_attack : MonoBehaviour
         CallDrrowsScale();
         DoAttackDelay = false;
         SetAttackDelay = 1;
+
+        Skill1SetCool = 7;//스킬1 쿨 설정
+        SetOnSkill1Time = 3;
+        DoSkill1 = false;
+        CoolOnSkill1 = false;
+        SendDoSkill1 = false;
     }
 
     // Update is called once per frame
@@ -49,11 +69,25 @@ public class drows_attack : MonoBehaviour
                     DoAttackDelay = true;
                 }
             }
-            
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                Rotaion = 0;
+            }
            
         }
+
+
+        //스킬1실행
+        if (Input.GetKey(KeyCode.Z))
+        {
+            AttackReady();
+            AttackReadyCool();
+        }
+
+        AttackReadyCool();
+
         //공격합수 반복
-        if(OnAtack==true)
+        if (OnAtack==true)
         {
             attack();
             print("OnAtack:");
@@ -70,6 +104,19 @@ public class drows_attack : MonoBehaviour
                 LeftBackAddRotation();
             }
 
+        }
+
+        if (OnSkill1 == true)
+        {
+            기본칼.SetActive(false);
+            공격준비칼.SetActive(true);
+            
+        }
+
+        if (OnSkill1 == false)
+        {
+            기본칼.SetActive(true);
+            공격준비칼.SetActive(false);
         }
 
         if (DoAttackDelay == true)
@@ -159,6 +206,57 @@ public class drows_attack : MonoBehaviour
     {
         var DrowsScript= GameObject.Find("drows").GetComponent<MovingObject>();
         DrowScale = DrowsScript.SendScaleX;
+    }
+
+
+
+
+
+    void AttackReady()//스킬1 함수
+    {
+        if (CoolOnSkill1 == false)//스킬1 실행
+        {
+            CoolSkill1 = Skill1SetCool;//스킬1 쿨 설정
+            DoSkill1 = true;//스킬1 지속 on
+            CoolOnSkill1 = true;//스킬1 쿨 on
+            OnSkill1Time = SetOnSkill1Time;//스킬1 지속시간 설정
+            SendDoSkill1 = true;
+        }
+    }
+
+    void AttackReadyCool()
+    {
+
+        //스킬 1 지속 시간 설정
+        if (DoSkill1 == true)
+        {
+            OnSkill1Time -= Time.deltaTime;
+            OnSkill1 = true;
+        }
+        else
+        {
+
+        }
+
+        if (OnSkill1Time <= 0)
+        {
+            DoSkill1 = false;
+            OnSkill1Time = 0;
+            SendDoSkill1 = false;
+            OnSkill1 = false;
+        }
+
+        //스킬1 쿨타임 설정
+        if (CoolOnSkill1 == true)
+        {
+            CoolSkill1 -= Time.deltaTime;
+        }
+
+        if (CoolSkill1 <= 0)
+        {
+            CoolOnSkill1 = false;
+            CoolSkill1 = 0;
+        }
     }
 
 }
